8bbbd804f0174d7386a7dbbeb1bf536ca6dae0c4
khasanov2oo9 <fazliddinhasanov34@gmail.com>
2025-06-26 15:50:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function setLightMode() {
+  document.body.classList.remove("dark-mode");
+  document.body.classList.add("light-mode");
+}
+
+function setDarkMode() {
+  document.body.classList.remove("light-mode");
+  document.body.classList.add("dark-mode");
+}
